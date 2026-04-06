1cfcd5d101a073fcd7ea3a50c81f1ba0cdfae996
Sinchana MP <sinchanamp3@gmail.com>
2026-04-06 21:42:50 +0530

    Add files via upload

---
+++ electricity.py
@@ -0,0 +1,11 @@
+units = int(input("Enter electricity units: "))
+
+if units <= 100:
+    bill = units * 1.5
+elif units <= 200:
+    bill = (100 * 1.5) + (units - 100) * 2.5
+elif units <= 300:
+    bill = (100 * 1.5) + (100 * 2.5) + (units - 200) * 4
+else:
+    bill = (100 * 1.5) + (100 * 2.5) + (100 * 4) + (units - 300) * 6
+print("Total bill:", bill)
