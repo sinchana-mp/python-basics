c75bb2d6458c4481d274d9341b7206e7ece13cc6
Sinchana MP <sinchanamp3@gmail.com>
2026-05-26 14:16:03 +0530

    Add files via upload

---
+++ student.class.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def display(self):
+        print("Name:", self.name)
+        print("Marks:", self.marks)
+
+student1 = Student("Sinchana", 95)
+
+student1.display()
